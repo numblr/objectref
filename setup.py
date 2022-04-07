9f818ce802f4c1d804f83c8648afcf42853f4262
Thomas Baier <tbaier@math.bme.hu>
2022-04-07 12:25:00 +0200

    Rename project name in setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("VERSION", "r") as fh:
     version = fh.read().strip()
 
 setup(
-    name='cltl.eliza',
+    name='cltl.leolani',
     version=version,
     package_dir={'': 'src'},
     packages=find_namespace_packages(include=['cltl.*', 'cltl_service.*'], where='src'),
     data_files=[('VERSION', ['VERSION'])],
-    url="https://github.com/leolani/cltl-eliza",
+    url="https://github.com/leolani/cltl-leolani",
     license='MIT License',
     author='CLTL',
     author_email='t.baier@vu.nl',
-    description='Eliza component for Leolani',
+    description='Temporary Leolani component',
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.7',
