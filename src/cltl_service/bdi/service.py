9210b6ddc5b1c18ff9213f79f96f1956325bf4f0
Thomas Baier <tbaier@math.bme.hu>
2022-06-08 12:45:52 +0200

    Pull BDI model out of BDIService

--- src/cltl_service/bdi/service.py
+++ src/cltl_service/bdi/service.py
@@ -1,19 +1,10 @@
 import logging
-import pathlib
 
-from cltl.brain import LongTermMemory
-from cltl.combot.event.emissor import TextSignalEvent
 from cltl.combot.event.bdi import IntentionEvent
 from cltl.combot.infra.config import ConfigurationManager
 from cltl.combot.infra.event import Event, EventBus
 from cltl.combot.infra.resource import ResourceManager
 from cltl.combot.infra.topic_worker import TopicWorker
-from cltl.reply_generation import LenkaReplier
-from cltl.triple_extraction.api import Chat
-from cltl.triple_extraction.cfg_analyzer import CFGAnalyzer
-from emissor.representation.scenario import TextSignal
-
-from cltl.leolani import emissor_api, talk
 
 logger = logging.getLogger(__name__)
 
@@ -30,13 +21,14 @@ class BDIService:
     """
 
     @classmethod
-    def from_config(cls, event_bus: EventBus, resource_manager: ResourceManager, config_manager: ConfigurationManager):
+    def from_config(cls, bdi_model: dict, event_bus: EventBus, resource_manager: ResourceManager,
+                    config_manager: ConfigurationManager):
         config = config_manager.get_config("cltl.bdi")
 
-        return cls(config.get("topic_scenario"), config.get("topic_intention"), config.get("topic_desire"),
+        return cls(bdi_model, config.get("topic_scenario"), config.get("topic_intention"), config.get("topic_desire"),
                    event_bus, resource_manager)
 
-    def __init__(self, scenario_topic: str, intention_topic: str, desire_topic: str,
+    def __init__(self, bdi_model: dict, scenario_topic: str, intention_topic: str, desire_topic: str,
                  event_bus: EventBus, resource_manager: ResourceManager):
         self._event_bus = event_bus
         self._resource_manager = resource_manager
@@ -50,13 +42,7 @@ class BDIService:
         self._scenario = None
         self._intentions = []
 
-        self._bdi = {"init":
-                         {"initialized": ["g2ky"]},
-                     "g2ky":
-                         {"resolved": ["chat"]},
-                     "chat":
-                         {"quit": ["init"]}
-                     }
+        self._bdi = bdi_model
 
     @property
     def app(self):
